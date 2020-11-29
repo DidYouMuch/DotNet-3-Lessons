aae395b6aa67fdcd79466cd56066982c3654af3b
DidYouMuch <toha.kom.in@gmail.com>
2020-11-29 19:31:40 +0300

    Task

---
+++ TaskApp/TaskApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task taskA = new Task(() => plus());
+            taskA.Start();
+            Console.ReadLine();
+        }
+
+        static void plus() {
+            int sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
